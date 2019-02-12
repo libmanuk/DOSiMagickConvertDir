829391f9b44309436abcd4d1b0baff02734d1ae7
Eric C. Weig <eweig@email.uky.edu>
2019-02-12 12:54:16 -0500

    Update im_conv_tif_dir.bat

--- im_conv_tif_dir.bat
+++ im_conv_tif_dir.bat
@@ -1,3 +1,9 @@
 @echo off
+title DOSiMagickConvertDir, Eric C. Weig 2019
+echo Using ImageMagick to convert all TIF images in current directory to JPG with:
+echo 1080 pixels on the long edge
+echo 300 dpi
+echo 80% compression rate
 for %%i in (*.tif) do magick convert -format jpg -quality 80%% -density 300 -units pixelsperinch -resize 1080^> %%~ni.tif %%~ni.jpg
+echo All done!  :)
 pause
