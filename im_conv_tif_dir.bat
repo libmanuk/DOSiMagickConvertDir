5cfd721b265ee0f5d08d9112b3e82ae809e73c19
Eric C. Weig <eweig@email.uky.edu>
2019-02-12 13:36:47 -0500

    Update im_conv_tif_dir.bat

--- im_conv_tif_dir.bat
+++ im_conv_tif_dir.bat
@@ -4,8 +4,21 @@ echo Using ImageMagick to convert all TIF images in current directory to JPG wit
 echo 1080 pixels on the long edge
 echo 300 dpi
 echo 80% compression rate
+pause
 echo File to convert:
 for %%i IN (*.tif) do echo %%i
+echo processing files, please wait...
 for %%i in (*.tif) do magick convert -flatten -format jpg -quality 80%% -density 300 -units pixelsperinch -resize 1080^> %%~ni.tif %%~ni.jpg
+echo.
+set tcount=0
+for %%x in (*.tif) do set /a tcount+=1
+echo found %tcount% tif images
+echo.
+set jcount=0
+for %%x in (*.jpg) do set /a jcount+=1
+echo created %jcount% jpg images
+echo.
+if %jcount% equ %tcount% ( echo Looks like all files were processed. ) else ( echo Looks like some files were not processed. )
+echo.
 echo All done!  :)
 pause
